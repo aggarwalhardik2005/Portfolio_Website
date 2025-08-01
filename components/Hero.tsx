c6fb0d0623858381dcfbc24fda3099c98982288c
aggarwalhardik2005 <hardikaggarwal01@gmail.com>
2025-08-01 11:14:31 +0530

    linkedin update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -43,7 +43,7 @@ export default function Hero() {
               </a>
             </Button>
             <Button variant="outline" size="lg" className="border-gray-300 bg-transparent" asChild>
-              <a href="https://linkedin.com/in/hardik-aggarwal" target="_blank" rel="noopener noreferrer">
+              <a href="https://www.linkedin.com/in/hardik-aggarwal-039648284/" target="_blank" rel="noopener noreferrer">
                 <Linkedin className="w-5 h-5 mr-2" />
                 LinkedIn
               </a>
